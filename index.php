154cdf0bb291de73c403e21bcdcfe5f515819d2e
Reza <rm.biqarar@gmail.com>
2020-04-08 19:10:39 +0430

    add some api function

--- index.php
+++ index.php
@@ -4,27 +4,43 @@
  * This function work by version 10 of jibres core api
  * @see https://core.jibres.com/r10/doc
  */
-
-class jibres_domain
+class Jibres_domain
 {
 
 	private $result_raw = [];
 
 
-	private function run($_path, $_method, $_param = null, $_body = null)
+	private function run($_path, $_method, $_param = null, $_body = null, $_option = [])
 	{
-
-		$appkey    = '[YOUR APP KEY]';
-		$apikey    = '[YOUR API KEY]';
-		$registrar = '[Domain registrar]';
+		$appkey     = '[YOUR APP KEY]';
+		$apikey     = '[YOUR API KEY]';
+		$registrar  = '[Domain registrar]';
 		$master_url = "https://core.jibres.com/%s/%s/%s";
 
-		$header =
+		$default_option =
 		[
-        	'Content-Type:application/json',
-        	'appkey: '. $appkey,
-        	'apikey: '. $apikey,
+			'apikey' => true,
 		];
+
+		if(!is_array($_option))
+		{
+			$_option = [];
+		}
+
+		$_option = array_merge($default_option, $_option);
+
+
+		// set headers
+		$header   = [];
+		$header[] = 'Content-Type:application/json';
+		$header[] = 'appkey: '. $appkey;
+
+		// set header apikey if need
+		if($_option['apikey'])
+		{
+			$header[] = 'apikey: '. $apikey;
+		}
+
 
 		$url = sprintf($master_url, 'r10', $registrar, $_path);
 
@@ -45,13 +61,12 @@ class jibres_domain
 		}
 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 20);
 
 		$response = curl_exec($ch);
-
 		$CurlError = curl_error($ch);
 
 		curl_close ($ch);
@@ -73,13 +88,15 @@ class jibres_domain
 			return false;
 		}
 
+		$this->result_raw = $result;
+
+		$this->make_notif();
+
 		if(!$result['ok'])
 		{
 			// build error
 			return false;
 		}
-
-		$this->result_raw = $result;
 
 		if(isset($result['result']))
 		{
@@ -87,6 +104,32 @@ class jibres_domain
 		}
 
 		return false;
+	}
+
+
+	private function make_notif()
+	{
+		if(empty($this->result_raw))
+		{
+			return;
+		}
+
+		$msg = [];
+
+		if(isset($this->result_raw['msg']) && is_array($this->result_raw['msg']))
+		{
+			$msg = $this->result_raw['msg'];
+		}
+
+		$notif = [];
+
+		foreach ($msg as $key => $value)
+		{
+			if(isset($value['text']) && isset($value['type']) && in_array($value['type'], ['info','ok','warn','error']))
+			{
+				// make notif
+			}
+		}
 	}
 
 
@@ -107,12 +150,18 @@ class jibres_domain
 		return null;
 	}
 
-	/**
-	 * get contact list
-	 */
+
+	// ---------------------------------------- CONTACT ---------------------------------------- //
 	public function contact_fetch()
 	{
 		$result = self::run('contact/fetch', 'get');
+		return $result;
+	}
+
+
+	public function contact_fetch_all()
+	{
+		$result = self::run('contact/fetch', 'get', ['all' => true]);
 		return $result;
 	}
 
@@ -148,6 +197,155 @@ class jibres_domain
 	public function contact_create_new($_args)
 	{
 		$result = self::run('contact/create', 'post', null, $_args);
+		return $result;
+	}
+
+
+	// ---------------------------------------- DOMAIN ---------------------------------------- //
+
+	public function domain_check($_domin)
+	{
+		$result = self::run('domain/check', 'get', ['domain' => $_domin], null, ['apikey' => false]);
+		return $result;
+	}
+
+
+	public function domain_fetch($_param)
+	{
+		$result = self::run('domain/fetch', 'get', $_param);
+		return $result;
+	}
+
+
+	public function domain_available($_domin)
+	{
+		$result = self::run('domain/available', 'get', ['domain' => $_domin], null, ['apikey' => false]);
+		return $result;
+	}
+
+
+	public function domain_detail($_domin)
+	{
+		$result = self::run('domain/detail', 'get', ['domain' => $_domin]);
+		return $result;
+	}
+
+
+	public function domain_autorenew($_domin_id, $_autorenew)
+	{
+		$result = self::run('domain', 'patch', ['id' => $_domin_id], ['autorenew' => $_autorenew]);
+		return $result;
+	}
+
+
+	public function domain_update_holder($_domin_id, $_args)
+	{
+		$result = self::run('domain/holder', 'patch', ['id' => $_domin_id], $_args);
+		return $result;
+	}
+
+
+	public function domain_update_dns($_domin_id, $_args)
+	{
+		$result = self::run('domain/dns', 'patch', ['id' => $_domin_id], $_args);
+		return $result;
+	}
+
+
+	public function domain_delete($_domin_id)
+	{
+		$result = self::run('domain', 'delete', ['id' => $_domin_id]);
+		return $result;
+	}
+
+
+	public function domain_lock($_domin_id)
+	{
+		$result = self::run('domain', 'put', ['id' => $_domin_id], ['lock' => true]);
+		return $result;
+	}
+
+
+	public function domain_unlock($_domin_id)
+	{
+		$result = self::run('domain', 'put', ['id' => $_domin_id], ['lock' => false]);
+		return $result;
+	}
+
+
+	public function domain_action($_domin_id)
+	{
+		$result = self::run('domain/action', 'get', ['id' => $_domin_id]);
+		return $result;
+	}
+
+
+	public function domain_buy($_args)
+	{
+		$result = self::run('domain/buy', 'post', null, $_args);
+		return $result;
+	}
+
+	public function domain_renew($_args)
+	{
+		$result = self::run('domain/renew', 'post', null, $_args);
+		return $result;
+	}
+
+
+	public function domain_transfer($_args)
+	{
+		$result = self::run('domain/transfer', 'post', null, $_args);
+		return $result;
+	}
+
+
+	// ---------------------------------------- POLL ---------------------------------------- //
+	public function poll_fetch()
+	{
+		$result = self::run('poll', 'get');
+		return $result;
+	}
+
+	// ---------------------------------------- DNS ---------------------------------------- //
+	public function dns_fetch()
+	{
+		$result = self::run('dns/fetch', 'get');
+		return $result;
+	}
+
+
+	public function dns_fetch_all()
+	{
+		$result = self::run('dns/fetch', 'get', ['all' => true]);
+		return $result;
+	}
+
+
+	public function dns_load($_id)
+	{
+		$result = self::run('dns', 'get', ['id' => $_id]);
+		return $result;
+	}
+
+
+	public function dns_remove($_id)
+	{
+		$result = self::run('dns', 'delete', ['id' => $_id]);
+		return $result;
+	}
+
+
+	public function dns_edit($_args, $_id)
+	{
+		$result = self::run('dns', 'patch', ['id' => $_id], $_args);
+		return $result;
+	}
+
+
+	public function dns_create($_args)
+	{
+		$result = self::run('dns/create', 'post', null, $_args);
 		return $result;
 	}
 
